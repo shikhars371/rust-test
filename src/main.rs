2f0a5da7e7c174b4735771f53e9e35314e8d7e7c
Shikhar Srivastava <shikhars371@gmail.com>
2024-01-08 23:06:15 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+#[allow(dead_code)]
 struct Object {
     width:u32,
     height:u32
@@ -22,14 +24,57 @@ impl Object {
         }
     }
 }
+#[derive(Debug)]
 
+enum Direction{
+    Up(Point),
+    Down(Point),
+    Left(Point),
+    Right(Point)
+}
+#[derive(Debug)]
+
+enum Keys{
+    Upkey(String),
+    Downkey(String),
+    Leftkey(String),
+    Rightkey(String)
+}
+impl Direction {
+    fn match_direction(&self)->Keys{
+        match *self {
+            Direction::Up(_) => Keys::Upkey(String::from("Pressed w")),
+            Direction::Down(_) => Keys::Downkey(String::from("Pressed s")),
+            Direction::Left(_) => Keys::Leftkey(String::from("Pressed a")),
+            Direction::Right(_) => Keys::Rightkey(String::from("Pressed d")),
+
+        }
+    }
+}
+
+impl Keys{
+    fn destruct(&self) -> &String{
+        match *self {
+            Keys::Upkey(ref s) => s,
+            Keys::Downkey(ref s) => s,
+            Keys::Leftkey(ref s) => s,
+            Keys::Rightkey(ref s) => s,
+            
+        }
+    }
+}
+#[derive(Debug)]
+
+struct Point{
+    x:i32,
+    y:i32
+}
 fn main(){
-    let o = Object {
-        width: 10,
-        height: 20
-    };
+    let u = Direction :: Up(Point{x:1,y:0});
+    let k = u.match_direction();
+    let x = k.destruct();
+    println!("{:?}",k);
 
-    o.show();
-    let obj = Object :: new(2,3);
-    obj.show();
+    println!("{}",x);
+
 }
